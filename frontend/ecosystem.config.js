ca2229a448cf4b7c2f2d14f92a040392adafc5f1
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-03-21 17:43:42 +0800

    Update ecosystem.config.js

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,7 +7,6 @@ module.exports = {
       API_URL: "http://localhost:8080",
       PORT: 3030
     },
-    log_file: "/var/www/pingvin_data/p_s_f.log",
-    instances: 0
+    log_file: "/var/www/pingvin_data/p_s_f.log"
   }]
 };
