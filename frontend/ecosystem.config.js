3b9215bfe1d55a2e3d1d9003673a1e20491b3b1f
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-03-21 14:03:16 +0800

    Create ecosystem.config.js

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: "pingvin-frontend",
+    script: "npm",
+    args: "run start",
+    watch: true,
+    env: {
+      API_URL: "http://localhost:8080",
+      PORT: 3030
+    },
+    log_file: "/var/www/pingvin_data/p_s_f.log",
+    instances: 0,
+    exec_mode: "cluster"
+  }]
+};
