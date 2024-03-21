b486c1c69fd68e83971d4c29bebdd740df699d4c
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-03-21 13:56:57 +0800

    Create ecosystem.config.js

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name: "pingvin-backend",
+    script: "npm",
+    args: "run prod",
+    watch: true,
+    env: {
+      PORT: 8080,
+      DATABASE_URL: "file:/var/www/pingvin_data/pingvin-share.db?connection_limit=1",
+      DATA_DIRECTORY: "/var/www/pingvin_data"
+    },
+    log_file: "/var/www/pingvin_data/p_s_b.log",
+    instances: 0,
+    exec_mode: "cluster"
+  }]
+};
