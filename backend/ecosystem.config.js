d008dc8e42bbf33b74c9ffa1f14b74cf0515672e
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-03-21 17:43:53 +0800

    Update ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,7 +8,6 @@ module.exports = {
       DATABASE_URL: "file:/var/www/pingvin_data/pingvin-share.db?connection_limit=1",
       DATA_DIRECTORY: "/var/www/pingvin_data"
     },
-    log_file: "/var/www/pingvin_data/p_s_b.log",
-    instances: 0
+    log_file: "/var/www/pingvin_data/p_s_b.log"
   }]
 };
