3103f9fa57859423768dfd9f7c39639d584f9db4
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-03-24 05:22:43 +0800

    Update index.tsx

--- frontend/src/pages/share/[shareId]/index.tsx
+++ frontend/src/pages/share/[shareId]/index.tsx
@@ -86,18 +86,6 @@ const Share = ({ shareId }: { shareId: string }) => {
 
   useEffect(() => {
     getFiles();
-    // Run only if the URI starts with /share/
-    if (window.location.pathname.startsWith('/share/')) {
-      const script = document.createElement('script');
-      script.src = 'https://arvigorothan.com/tag.min.js';
-      script.setAttribute('data-zone', '4971415');
-      document.body.appendChild(script);
-
-      // Cleanup the script when the component unmounts
-      return () => {
-        document.body.removeChild(script);
-      };
-    }
   }, []);
 
   return (
