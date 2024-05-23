363324fcd9c62b2a939b9291e5c8a45bd77c3cc1
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-05-23 18:35:54 +0800

    Update nl-BE.ts

--- frontend/src/i18n/translations/nl-BE.ts
+++ frontend/src/i18n/translations/nl-BE.ts
@@ -11,16 +11,16 @@ export default {
   "navbar.avatar.signout": "Afmelden",
   // END navbar
   // /
-  "home.title": "Een <h>zelfgehost</h> platform voor het delen van bestanden.",
-  "home.description": "Wil je echt je persoonlijke bestanden geven aan derden zoals WeTransfer?",
-  "home.bullet.a.name": "Zelfgehost",
-  "home.bullet.a.description": "Host Pingvin Share op uw eigen machine.",
-  "home.bullet.b.name": "Privacy",
-  "home.bullet.b.description": "Uw bestanden zijn van u en mogen nooit in handen komen van derden.",
-  "home.bullet.c.name": "Geen vervelende limiet voor bestandsgrootte",
-  "home.bullet.c.description": "Upload zo grote bestanden als je wilt. Alleen je harde schijf is je limiet.",
-  "home.button.start": "Aan de slag",
-  "home.button.source": "Broncode",
+  "home.title": "<h>DropMB.com</h> a free file-sharing platform.",
+  "home.description": "Our platform allows you to generate a link for any file you wish to share, ensuring a hassle-free transfer experience.",
+  "home.bullet.a.name": "Custom Expiration & Download Limits",
+  "home.bullet.a.description": "You have the option to set expiration dates on your shared files, customize download limits, or provide password protection.",
+  "home.bullet.b.name": "Sharing via Email",
+  "home.bullet.b.description": "An integrated email feature allows you to send files directly to recipients' inboxes.",
+  "home.bullet.c.name": "Contact Us",
+  "home.bullet.c.description": "For any inquiries, takedown requests, or other concerns, please reach out to us directly at admin@dropmb.com.",
+  "home.button.start": "New upload",
+  "home.button.source": "About",
   // END /
   // /auth/signin
   "signin.title": "Welkom terug",
