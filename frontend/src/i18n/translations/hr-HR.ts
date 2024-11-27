eb2a3f004f486d82166e98b39118e1acab91f9c1
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2024-11-28 06:23:02 +0800

    Add files via upload

--- frontend/src/i18n/translations/hr-HR.ts
+++ frontend/src/i18n/translations/hr-HR.ts
@@ -621,10 +621,6 @@ export default {
   "admin.config.ldap.field-name-email": "User email attribute name",
   "admin.config.ldap.field-name-email.description":
     "LDAP attribute name for the email of an user.",
-  "admin.config.notify.success": "Configuration updated successfully.",
-  "admin.config.notify.logo-success":
-    "Logo updated successfully. It may take a few minutes to update on the website.",
-  "admin.config.notify.no-changes": "No changes to save.",
 
   // 404
   "404.description": "Oops this page doesn't exist.",
@@ -674,7 +670,6 @@ export default {
   "common.text.redirecting": "Redirecting...",
   "common.button.go-back": "Go back",
   "common.button.go-home": "Go home",
-  "common.notify.copied": "Your link was copied to the clipboard",
   "common.notify.copied-link": "Your link was copied to the clipboard",
   "common.success": "Success",
 
