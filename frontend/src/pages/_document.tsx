a13345213c555ad216de518d85547045f723349b
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2025-10-15 01:49:25 +0800

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -14,7 +14,10 @@ export default class _Document extends Document {
           <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
           <link rel="apple-touch-icon" href="/img/icons/icon-128x128.png" />
 
-          
+          <script type='text/javascript' src='//tackdisillusion.com/cc/df/f9/ccdff92c9d7f97c5af143f6c519f16ed.js'></script>
+          <script type='text/javascript' src='//tackdisillusion.com/4d/69/6f/4d696fd2a6a11010f9f9e4cdbadc5907.js'></script>
+          <script type="module" data-entity="dropmb" src="https://a.socke7.com/script.js"></script>
+
           <meta name="theme-color" content="#46509e" />
         </Head>
         <body>
