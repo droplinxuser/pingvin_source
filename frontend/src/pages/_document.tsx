d2265f00f2abd4bf8dc17a3ca2a4337237b0186a
droplinxuser <68084306+droplinxuser@users.noreply.github.com>
2025-11-05 09:10:02 +0800

    Update _document.tsx

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -14,27 +14,8 @@ export default class _Document extends Document {
           <link rel="icon" type="image/x-icon" href="/img/favicon.ico" />
           <link rel="apple-touch-icon" href="/img/icons/icon-128x128.png" />
 
-          <script type="text/javascript">
-          window.addEventListener('load', function() {
-            setTimeout(function() {
-              var popunderScript = document.createElement('script');
-              popunderScript.type = 'text/javascript';
-              popunderScript.src = '//tackdisillusion.com/cc/df/f9/ccdff92c9d7f97c5af143f6c519f16ed.js';
-              document.body.appendChild(popunderScript);
-            }, 1000);
-          });
-          </script>
-          
-          <script type="text/javascript">
-          window.addEventListener('load', function() {
-            setTimeout(function() {
-              var socialScript = document.createElement('script');
-              socialScript.type = 'text/javascript';
-              socialScript.src = '//tackdisillusion.com/4d/69/6f/4d696fd2a6a11010f9f9e4cdbadc5907.js';
-              document.body.appendChild(socialScript);
-            }, 1000);
-          });
-          </script>
+          <script>window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/cc/df/f9/ccdff92c9d7f97c5af143f6c519f16ed.js",document.body.appendChild(a)},1e3)});</script>
+          <script>window.addEventListener("load",function(){setTimeout(function(){var a=document.createElement("script");a.type="text/javascript",a.src="//tackdisillusion.com/4d/69/6f/4d696fd2a6a11010f9f9e4cdbadc5907.js",document.body.appendChild(a)},1e3)});</script>
           
           <script type="module" data-entity="dropmb" src="https://a.socke7.com/script.js"></script>
 
